381df4d7ea12b5fde286626f4e81e6fec6d6dfad
santobuonoivan <48569222+santobuonoivan@users.noreply.github.com>
2019-03-26 20:45:07 -0300

    modelado

--- RandController.php
+++ RandController.php
@@ -5,15 +5,18 @@
 
 class RandController implements ControllerInterface
 {
+    
     public static function get()
     {
         return 'un numero random por GET: '. rand(1,99999999);
     }
     public static function post()
     {
+        $min = isset($_POST['min']) ? $_POST['min'] : 1;
+        $max = isset($_POST['max']) ? $_POST['max'] : 10;
         $array = [];
         $array['status'] = 200;
-        $array['message'] = 'un numero random por POST: '. rand(1,100);
+        $array['message'] = 'un numero random por POST entre '.$min.' - '.$max.': '.  rand( $min, $max);
         return json_encode($array);
     }
 }
--- index.php
+++ index.php
@@ -11,15 +11,19 @@ $miFramework = new MiFramework;
 //RUTES
 
 $miFramework->setRute('GET','/', RandController::class);
+// POST espera min y max. si no están es 1 y 10 por defecto
 $miFramework->setRute('POST','/', RandController::class);
+
 // Agregar mas controllers
 
 
 //tomo el método utilizado para pegarle al framework
 $method = $_SERVER['REQUEST_METHOD'];
+//var_dump($_POST);
 
 // Según el metodo utilizado busco qeu devolver
 // Según que metodo llamo a las funciones setadas
+
 switch ($method) {
     case 'GET':
         echo $miFramework->get('/');
@@ -32,6 +36,7 @@ switch ($method) {
         # code...
         break;
 }
+
 
 
 
